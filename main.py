47dfceedd3bfea1fb20a2e678e8388783f651968
Armen Martirosyan <armartirosyan@gmail.com>
2017-01-30 18:24:01 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
+import os
 from flask import Flask
-from flask import render_template
 
 app = Flask(__name__)
-@app.route('/')
-def register():
-    return render_template('index.html')
 
-
-def main():
-    app.run(host='0.0.0.0', port=5000)
+@app.route("/")
+def hello():
+    return "Hello world!"
 
 if __name__ == "__main__":
-    main()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)

b67c8d70164611b6be46c6485f956a8d372292d3
Armen Martirosyan <armartirosyan@gmail.com>
2017-01-31 00:56:46 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
+# Initialize the Flask application
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello world!"
+# Define a route for the default URL, which loads the form
+@app.route('/')
+def index():
+    return render_template('index.html')
 
+
+@app.route('/oauth')
+def oauth():
+    return render_template('oauth.html')
+
+# Run the app
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
